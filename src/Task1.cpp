206b6adac1065fcf9a0919ea8704cab6e2b36641
riya-bubna <bubna.riya13@gmail.com>
2015-03-15 03:44:46 +0530

    I want Soumyadeep

--- src/Task1.cpp
+++ src/Task1.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 double X;
 double Y;
+double x_start;
+double y_start;
+int state=2;
 
 void odomcallBack(nav_msgs::Odometry msg)
 {
@@ -24,53 +27,46 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   ros::Publisher vel_pub_0 = n.advertise<geometry_msgs::Twist>("/swarmbot0/cmd_vel",1);
   ros::Subscriber sub=n.subscribe<nav_msgs::Odometry>("/swarmbot0/odom",1000,odomcallBack);
-
+  geometry_msgs::Twist cmd_vel;
+  
   int count = 0;
-  ros::Rate loop_rate(5);
- 
+  double z=1.8;
+  ros::Rate loop_rate(10);
 
   while (ros::ok())
   {
-    geometry_msgs::Twist cmd_vel;
-    
-//    double x_start = X;
- //   double y_start= Y;
-    cmd_vel.linear.x = 2.0;
-    cmd_vel.linear.y = 0;
-    cmd_vel.linear.z= 0;
-	cmd_vel.angular.x = 0;
-	cmd_vel.angular.y = 0;
-
-    cmd_vel.angular.z = 1.8;
-/*
-    while((abs(x_start-X)<0.001) &&(abs(y_start-Y)<0.001 ));
-    while((abs(x_start-X)>0.001) &&(abs(y_start-Y)>0.001 ));
-    cmd_vel.linear.x = 0;
-    cmd_vel.linear.y = 0;
-    cmd_vel.angular.z = 0;
-
-    x_start = X;
-    y_start = Y;
-
-    cmd_vel.linear.x = 2;
-    cmd_vel.linear.y = 0;
-    cmd_vel.angular.z = -1.8;
-	
-    while((abs(x_start-X)<0.001) &&(abs(y_start-Y)<0.001 ));
-    while((abs(x_start-X)>0.001) &&(abs(y_start-Y)>0.001 ));
-    cmd_vel.linear.x = 0;
-    cmd_vel.linear.y = 0;
-    cmd_vel.angular.z = 0;
-	*/
+  	
+  	cmd_vel.linear.x = 2;
+  	cmd_vel.linear.y = 0;
+  	cmd_vel.linear.z = 0;
+  	cmd_vel.angular.x = 0;
+  	cmd_vel.angular.y = 0;
+  	if(state==2)
+  	{
+  		x_start = X;
+    	y_start = Y;
+    	cmd_vel.angular.z = z;
+  		printf("state- %d z: %lf \n", state,z);
+    }
+    if((abs(x_start-X)<0.01) &&(abs(y_start-Y)<0.01 ))
+  	{
+  		if(state==0)
+  		{
+  			state=1;
+  			x_start = X;
+    		y_start = Y;
+    		z=-z;
+    		cmd_vel.angular.z = z;
+  			printf("state- %d z: %lf \n", state,z);
+    	}
+	}
+    else
+    	state=0;
 
     vel_pub_0.publish(cmd_vel);
-
-    ros::spin();
-
+    ros::spinOnce();
     loop_rate.sleep();
-
-    ++count;
-    //ros::spinOnce();
+    count++;
   }
   return 0;
 }
